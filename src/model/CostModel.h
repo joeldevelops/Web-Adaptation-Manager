2a335f1c1e67c48e05c50c55a239054d0fab943e
Joel Milligan <joelsaltwater@gmail.com>
2022-12-16 09:39:34 +0100

    Fix branch histories

---
+++ src/model/CostModel.h
@@ -0,0 +1,17 @@
+/*
+ * CostModel.h
+ *
+ *  Created on: Dec 15, 2022
+ *      Author: root
+ */
+
+#ifndef MODEL_COSTMODEL_H_
+#define MODEL_COSTMODEL_H_
+
+class CostModel {
+public:
+    CostModel();
+    virtual ~CostModel();
+};
+
+#endif /* MODEL_COSTMODEL_H_ */
