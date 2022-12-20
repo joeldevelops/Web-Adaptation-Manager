b0579f0f3e90d4d6fa4e242bce11919424e3faa1
Vlad2000Andrei <cursaru.vlad@gmail.com>
2022-12-20 10:52:15 +0000

    Created cost model

--- src/model/CostModel.cc
+++ src/model/CostModel.cc
@@ -7,12 +7,36 @@
 
 #include <model/CostModel.h>
 
-CostModel::CostModel() {
-    // TODO Auto-generated constructor stub
+Define_Module(CostModel)
 
+CostModel::CostModel() {
+    eModel = nullptr;
+    pollingIntervalLen = 1;
+}
+
+CostModel::CostModel(EnergyModel* eModel, double pollingIntervalLen) {
+    this->eModel = eModel;
+    this->pollingIntervalLen = pollingIntervalLen;
 }
 
 CostModel::~CostModel() {
     // TODO Auto-generated destructor stub
 }
 
+double CostModel::getCost() {
+    double powerDraw = eModel->getWallPowerDraw();
+    double energyUsed = powerToEnergy(powerDraw);
+    double cost = energyToCost(energyUsed);
+    return cost;
+}
+
+double CostModel::powerToEnergy(double powerDraw) {
+    constexpr double KWH_PER_SEC = 1.0 / 3600;
+    return powerDraw * pollingIntervalLen * KWH_PER_SEC;
+}
+
+double CostModel::energyToCost(double energyUsed) {
+    energyCost = ElectricityCostModel::getElectricityCost();
+    std::cout << "\neCOST: " << energyCost << "EUR/KWh\n";
+    return energyUsed;
+}
--- src/model/CostModel.h
+++ src/model/CostModel.h
@@ -8,10 +8,29 @@
 #ifndef MODEL_COSTMODEL_H_
 #define MODEL_COSTMODEL_H_
 
-class CostModel {
+#include "model/EnergyModel.h"
+#include <omnetpp.h>
+#include "model/ElectricityCostModel.h"
+
+class CostModel  : public omnetpp::cSimpleModule {
 public:
     CostModel();
+    CostModel(EnergyModel* eModel, double pollingIntervalLen);
     virtual ~CostModel();
+
+    double getCost();
+    double energyCost;
+
+
+private:
+    EnergyModel* eModel;
+    double adMarkup;
+    double adPriceRange;
+    double pollingIntervalLen; // in seconds
+
+    double powerToEnergy(double powerDraw);  // energy in KWh
+    double energyToCost(double energyUsed);
+
 };
 
 #endif /* MODEL_COSTMODEL_H_ */
