0072ab04bd6bb9e325c61d8cc5d06bf98551253c
Vlad2000Andrei <cursaru.vlad@gmail.com>
2022-12-21 08:05:38 +0000

    Made power cost thresholds more realistic

--- src/model/ElectricityCostModel.h
+++ src/model/ElectricityCostModel.h
@@ -23,8 +23,8 @@ class ElectricityCostModel {
 
 public:
     ElectricityCostModel();
-    static constexpr double max = 0.6;  // EUR per KWh
-    static constexpr double min = 0.2;  // EUR per KWh
+    static constexpr double max = 0.5;  // EUR per KWh
+    static constexpr double min = 0.1;  // EUR per KWh
 
     static double getElectricityCost();
     static double getElectricityCostNoUpdate();
